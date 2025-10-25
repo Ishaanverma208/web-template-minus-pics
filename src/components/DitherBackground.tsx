5f29685c45654bd6c6d861dc55ed95c4ee826467
Ishaanverma208 <ishaanverma208@gmail.com>
2025-10-25 14:25:46 +0530

    Update DitherBackground.tsx

--- src/components/DitherBackground.tsx
+++ src/components/DitherBackground.tsx
@@ -1,3 +1,17 @@
+import Dither from './Dither';
+
+<div style={{ width: '100%', height: '600px', position: 'relative' }}>
+  <Dither
+    waveColor={[0.5, 0.5, 0.5]}
+    disableAnimation={false}
+    enableMouseInteraction={true}
+    mouseRadius={0.3}
+    colorNum={4}
+    waveAmplitude={0.3}
+    waveFrequency={3}
+    waveSpeed={0.05}
+  />
+</div>
 /* eslint-disable react/no-unknown-property */
 import { useRef, useEffect, forwardRef } from 'react';
 import { Canvas, useFrame, useThree, ThreeEvent } from '@react-three/fiber';
